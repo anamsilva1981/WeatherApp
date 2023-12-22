337a23298a6b5a21de228676ef894b1583a292f2
ana-msilva <ana.msilva@ibm.com>
2023-12-22 08:01:55 -0300

    feat: add imagens, interface e consumo de servico

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  private weatherService = inject(WeatherService);
+
+  public initialCityName = 'São Paulo';
+  public weatherDatas!: any;
+
+  public ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName)
+  }
+
+  public getWeatherDatas(cityName: string): void{
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        console.log('Retorno de response: ', response)
+      },
+      error: (error) => {
+        console.log('Retorno de error', error);
+
+      }
+    })
+  }
 
 }
--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -8,11 +8,10 @@ import { Observable } from 'rxjs';
 export class WeatherService {
   private apiKey = 'd9c875157ce29908cab7e11dc74dd454';
   private http = inject(HttpClient);
-  private urlApi = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
   constructor() { }
 
   public getWeatherDatas(cityName: string): Observable<any>{
-    return this.http.get(`this.urlApi${cityName}&units=metrics&mode=json&appid=${this.apiKey}`, {})
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metrics&mode=json&appid=${this.apiKey}`, {})
   }
 }
